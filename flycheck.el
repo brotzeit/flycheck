765ac3e480a16b7ab252d03c624d68fe212f28e2
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-04-27 17:36:29 -0400

    Don't inherit from constant-face in error-list-{line,column}-number

--- flycheck.el
+++ flycheck.el
@@ -902,15 +902,14 @@ This variable is a normal hook.  See Info node `(elisp)Hooks'."
   :package-version '(flycheck . "0.16")
   :group 'flycheck-faces)
 
-;; The base faces for the following two faces are inspired by Compilation Mode
 (defface flycheck-error-list-line-number
-  '((t :inherit font-lock-constant-face))
+  '((t))
   "Face for line numbers in the error list."
   :group 'flycheck-faces
   :package-version '(flycheck . "0.16"))
 
 (defface flycheck-error-list-column-number
-  '((t :inherit font-lock-constant-face))
+  '((t))
   "Face for line numbers in the error list."
   :group 'flycheck-faces
   :package-version '(flycheck . "0.16"))
